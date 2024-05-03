35d83e0b76ed7809c68f25720deb63e8c4e6583e
Aarav Lodha <lodhaaarav12005@gmail.com>
2024-05-03 17:52:32 +0530

    added bubble sor

--- graph-coloring.c
+++ graph-coloring.c
@@ -3,17 +3,17 @@
 int main()
 {
     int n;
-    printf("Enter the number of bertices of graph\n");
+    printf("Enter the number of vertices of graph\n");
     scanf("%d", &n);
     int arr[n][n];
     for(int i=0; i<n; i++)
     {
         for(int j=0; j<n; j++)
         {
-            printf("Enter 1 if vertex %d and %d are connected else 0 :", i+1, j+1);
+            printf("Enter 1 if vertex %d and %d are connected else 0 : ", i+1, j+1);
             scanf("%d", &arr[i][j]);
         }
     }
-    
+
     return 0;
 }
