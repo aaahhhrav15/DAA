d4daa9ea7744ff41e994a91c4ff5df3737924a26
Aarav Lodha <lodhaaarav12005@gmail.com>
2024-05-17 20:00:48 +0530

    added user input to it

--- bellman_ford(Dynamic Programming).c
+++ bellman_ford(Dynamic Programming).c
@@ -7,12 +7,14 @@ void BellmanFord(int graph[][3], int V, int E, int src)
 {
     int dis[V];
 
+    // Initialization: Set all distances to infinity except the source vertex
     for (int i = 0; i < V; i++)
     {
         dis[i] = INF;
     }
     dis[src] = 0;
 
+    // Relax edges repeatedly for V-1 times
     for (int i = 0; i < V - 1; i++) 
     {
         for (int j = 0; j < E; j++) 
@@ -24,6 +26,7 @@ void BellmanFord(int graph[][3], int V, int E, int src)
         }
     }
 
+    // Check for negative weight cycles
     for (int i = 0; i < E; i++) 
     {
         int x = graph[i][0];
@@ -32,9 +35,11 @@ void BellmanFord(int graph[][3], int V, int E, int src)
         if (dis[x] != INF && dis[x] + weight < dis[y])
         {
             printf("Graph contains negative weight cycle\n");
+            return;
         }
     }
 
+    // Print shortest distances
     printf("Vertex Distance from Source\n");
     for (int i = 0; i < V; i++)
     {
@@ -44,14 +49,25 @@ void BellmanFord(int graph[][3], int V, int E, int src)
 
 int main()
 {
-    int V = 5;
-    int E = 8;
-    int graph[][3] = { { 0, 1, -1 }, { 0, 2, 4 },
-                        { 1, 2, 3 }, { 1, 3, 2 }, 
-                        { 1, 4, 2 }, { 3, 2, 5 }, 
-                        { 3, 1, 1 }, { 4, 3, -3 } };
+    int V, E;
+    printf("Enter the number of vertices: ");
+    scanf("%d", &V);
+    printf("Enter the number of edges: ");
+    scanf("%d", &E);
+    
+    int graph[E][3];
+    printf("Enter source, destination, and weight for each edge:\n");
+    for(int i = 0; i < E; i++)
+    {
+        printf("Edge %d: ", i + 1);
+        scanf("%d %d %d", &graph[i][0], &graph[i][1], &graph[i][2]);
+    }
 
-    BellmanFord(graph, V, E, 0);
+    int src;
+    printf("Enter the source vertex: ");
+    scanf("%d", &src);
+
+    BellmanFord(graph, V, E, src);
 
     return 0;
 }
